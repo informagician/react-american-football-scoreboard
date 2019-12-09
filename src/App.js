d63b7388c36171d7cb8d167d2b3b14f2f9644afa
Milo Rastgoo <hello@informagician.com>
2019-12-09 17:03:06 -0500

    Logic Separated - Stretch Goal 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, { useState } from "react";
 import "./App.css";
-import BottomRow from "./BottomRow";
+import ScoreBoard from "./ScoreBoard";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
@@ -10,23 +10,7 @@ function App() {
 
   return (
     <div className="container">
-      <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">{homeValue}</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awayValue}</div>
-          </div>
-        </div>
-        <BottomRow />
-      </section>
+      <ScoreBoard homeValue={homeValue} awayValue={awayValue}/>
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
